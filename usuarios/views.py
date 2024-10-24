9125d26b51a9b0228288aa2cca7d42bcf521888c
Nelson Ramirez <xhyskoax@gmail.com>
2024-10-24 20:57:25 -0300

    se crea vista registro y login de usuarios en app usuarios

---
+++ usuarios/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+
+# VISTA BASADA EN CLASE PARA REGISTRO
+class UserRegistroView(CreateView):
+    form_class =  UserCreationForm
+    template_name = 'usuarios/registro.html'
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Registro realizo con éxito.')
+        return super().form_valid(form)
+    
+    
+
+# VISTA BASADA EN CLASE PARA LOGIN
+class UserLoginView(LoginView):
+    template_name = 'usuarios/login.html'
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('index')
+
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Login exitoso.')
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        return self.get_redirect_url() or self.success_url
+    
+    
+#vista de logout
